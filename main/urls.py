f2d05d3edc80728fe4a8ae0b54238c250e811a82
VladyslavShaposhnikov <shapik2404@gmail.com>
2021-04-27 13:28:09 +0200

    create templates/categories.html and do migrations

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import index
+from .views import index, show_categories
 
 urlpatterns = [
     path('', index, name='index'),
+    path('categories/', show_categories, name='categories'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import Category
 
 def index(request):
     return render(request, 'index.html')
+
+def show_categories(request):
+    return render(request, "categories.html", {'categories': Category.objects.all()})

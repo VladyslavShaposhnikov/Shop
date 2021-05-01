53107d80576f478a54f28ef2ef45691a204c53ff
VladyslavShaposhnikov <shapik2404@gmail.com>
2021-05-01 14:06:57 +0200

    change categories.html (for test extends index.html)

--- main/views.py
+++ main/views.py
@@ -1,23 +1,32 @@
 from django.shortcuts import render
 from .models import Category
+from django.views.generic.detail import DetailView
+
+def get_cat(num):
+    cat = Category.objects.get(pk=num)
+    cats = cat.get_children()
+    return cats
+
+boys = get_cat(5)
+girls = get_cat(2)
+baby = get_cat(9)
+toys = get_cat(10)
+sport = get_cat(11)
 
 def index(request):
-    def get_cat(num):
-        cat = Category.objects.get(pk=num)
-        cats = cat.get_children()
-        return cats
-    boys = get_cat(5)
-    girls = get_cat(2)
-    baby = get_cat(9)
-    toys = get_cat(10)
-    sport = get_cat(11)
     return render(request, 'index.html', {
-        'boys':boys,
-        'girls':girls,
-        'baby':baby,
-        'toys':toys,
-        'sport':sport,
+        'boys'  :boys,
+        'girls' :girls,
+        'baby'  :baby,
+        'toys'  :toys,
+        'sport' :sport,
         })
 
 def show_categories(request):
-    return render(request, "categories.html", {'categories': Category.objects.all()})
+    return render(request, "categories.html", {'categories':Category.objects.all(),
+                                                'boys'     :boys,
+                                                'girls'    :girls,
+                                                'baby'     :baby,
+                                                'toys'     :toys,
+                                                'sport'    :sport,
+                                                })

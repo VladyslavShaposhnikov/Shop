a8795f2317fb652e356105c7d34ec5337e5c2ba2
VladyslavShaposhnikov <shapik2404@gmail.com>
2021-05-06 12:14:12 +0200

    change main page(index.html)

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from .models import *
+from mptt.admin import MPTTModelAdmin
 
 admin.site.register(Cart)
 admin.site.register(CartProduct)
 admin.site.register(Customer)
 admin.site.register(Product)
-admin.site.register(Category)
+admin.site.register(Category, MPTTModelAdmin)
 admin.site.register(BoyGirlBaby)

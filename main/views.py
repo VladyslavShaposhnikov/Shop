2fc9080b7d274632b22404ffd0464402944b947f
VladyslavShaposhnikov <shapik2404@gmail.com>
2021-05-11 13:22:08 +0200

    urls done

--- main/views.py
+++ main/views.py
@@ -39,7 +39,7 @@ class Productdetail(DetailView):
 }
     def dispatch(self, request, *args, **kwargs):
         self.model = self.GET_MODEL_SLUG[kwargs['ct_model']]
-        self.queryset = self.model._base_manager.all()
+        self.queryset = self.model.objects.all()
         return super().dispatch(request, *args, **kwargs)
 
     context_object_name = 'product'
@@ -55,6 +55,48 @@ class Productdetail(DetailView):
         context['sport'] = sport
         return context
 
+class Categorydetail(DetailView):
+    
+    GET_MODEL_SLUG = {
+    'boy':Boy,
+    'girl':Girl,
+    'baby':Baby,
+    'toys':Toys,
+    'sport':Sport
+}
+    def dispatch(self, request, *args, **kwargs):
+        self.model = self.GET_MODEL_SLUG[kwargs['ct_model']]
+        self.queryset = self.model.objects.all()
+        return super().dispatch(request, *args, **kwargs)
+
+    context_object_name = 'category'
+    template_name = 'category_detail.html'
+    slug_url_kwarg = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['boys'] = boys
+        context['girls'] = girls
+        context['baby'] = baby
+        context['toys'] = toys
+        context['sport'] = sport
+        return context
+
+Boys = ('shoes','blouse', 'jacket', 'underwear', 'socks', 'reglan', 'sportswear', 't-short', 'hat', 'pants')
+Girls = ('shoes-for-girl','blouse-for-girl', 'jacket-for-girl', 'dress', 'underwear-for-girl', 'reglan-for-girl', 'sportswear-for-girl', 't-short-for-girl', 'hat-for-girl', 'pants-for-girl', 'tights-for-girl')
+Toy = ('for-girls', 'for-boys', 'constructors', 'stuffed-animals', 'musical-toys', 'board-games', 'educational-toys')
+Spor = ('cycling', 'skateboards', 'sled', 'scooters')
 def categorydetail(request,slug):
     context['category'] = Category.objects.get(slug=slug)
+    if slug in Boys:
+        model = Boy
+    elif slug in Girls:
+        model = Girl
+    elif slug in Toy:
+        model = Toys
+    elif slug in Spor:
+        model = Sport
+    else:
+        model = Baby
+    context['cats'] = model.objects.filter(category__slug=slug)
     return render(request, 'category_detail.html', context)

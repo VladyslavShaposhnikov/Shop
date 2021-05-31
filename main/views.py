5d189424af97520117e19e7d1c0ac9315cff2f53
VladyslavShaposhnikov <shapik2404@gmail.com>
2021-05-31 12:00:24 +0200

    variables names

--- main/views.py
+++ main/views.py
@@ -7,22 +7,22 @@ from django.views.generic import View
 from django.http import HttpResponseRedirect
 from .forms import OrderForm
 
-def get_cat(num):
-    cat = Category.objects.get(pk=num)
-    cats = cat.get_children()
-    return cats
+def get_subcategory(num):
+    parent_category = Category.objects.get(pk=num)
+    subcategorys = parent_category.get_children()
+    return subcategorys
 
-boys = get_cat(5)
-girls = get_cat(2)
-baby = get_cat(9)
-toys = get_cat(10)
-sport = get_cat(11)
+boys = get_subcategory(5)
+girls = get_subcategory(2)
+baby = get_subcategory(9)
+toys = get_subcategory(10)
+sport = get_subcategory(11)
 
-def user_cart(cus):
-    if cus.is_authenticated:
-        customer = Customer.objects.filter(user=cus).first()
+def user_cart(current_user):
+    if current_user.is_authenticated:
+        customer = Customer.objects.filter(user=current_user).first()
         if not customer:
-            customer = Customer.objects.create(user=cus)
+            customer = Customer.objects.create(user=current_user)
         cart = Cart.objects.filter(customer=customer, in_order=False)
         if cart:
             cart = cart.get()
@@ -33,7 +33,6 @@ def user_cart(cus):
     else:
         cart = Cart.objects.filter(for_anonymous_user=True).first()
         if cart:
-            #cart = cart.get()
             return cart
         else:
             cart = Cart.objects.create(for_anonymous_user=True)
@@ -49,11 +48,9 @@ context = {
 
 def index(request):
     context['babies'] = Baby.objects.filter(father=None).order_by('-pub_date')
-    cus = request.user
-    c = user_cart(cus)
-    print(cus, c)
-    context['cart'] = c
-    #context['cart'] = Cart.objects.get(customer=cart_owner)
+    current_user = request.user
+    current_cart = user_cart(current_user)
+    context['cart'] = current_cart
     return render(request, 'index.html', context)
 
 def show_categories(request):
@@ -85,9 +82,9 @@ class Productdetail(DetailView):
         context['baby'] = baby
         context['toys'] = toys
         context['sport'] = sport
-        cus = self.request.user
-        c = user_cart(cus)
-        context['cart'] = c
+        current_user = self.request.user
+        current_cart = user_cart(current_user)
+        context['cart'] = current_cart
         context['size_of_prod'] = self.model.objects.get(slug=self.kwargs['slug'])
         return context
 
@@ -108,15 +105,15 @@ def categorydetail(request,slug):
         context['cats'] = Sport.objects.filter(category__slug=slug)
     else:
         context['cats'] = Baby.objects.filter(father=None, category__slug=slug)
-    cus = request.user
-    c = user_cart(cus)
-    context['cart'] = c
+    current_user = request.user
+    current_cart = user_cart(current_user)
+    context['cart'] = current_cart
     return render(request, 'category_detail.html', context)
 
 def cart(request):
-    cus = request.user
-    c = user_cart(cus)
-    context['cart'] = c
+    current_user = request.user
+    current_cart = user_cart(current_user)
+    context['cart'] = current_cart
     context['baby_size'] = Baby.objects.all()
     context['boy_size'] = Boy.objects.all()
     context['girl_size'] = Girl.objects.all()
@@ -124,20 +121,20 @@ def cart(request):
 
 def add_to_cart(request, *args, **kwargs):
     ct_model, slug = kwargs.get('ct_model'), kwargs.get('slug')
-    cus = request.user
-    c = user_cart(cus)
+    cart_owner = Customer.objects.get(user=request.user)
+    cart = Cart.objects.get(customer=cart_owner, in_order=False)
     prod = Product.objects.get(slug=slug)
     cart_prod, created = CartProduct.objects.get_or_create(
-        owner=c.customer,
-        basket=c,
+        owner=cart_owner,
+        basket=cart,
         product=prod
     )
     if created:
-        c.product_in_cart.add(cart_prod)
+        cart.product_in_cart.add(cart_prod)
         messages.add_message(request, messages.INFO, "'{}' додано до кошику".format(prod.title))
     else:
         messages.add_message(request, messages.INFO, "'{}' вже є у вашому кошику".format(prod.title))
-    recalculate_cart(c)
+    recalculate_cart(cart)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'), context)
 
 def delete_from_cart(request, *args, **kwargs):
@@ -186,21 +183,21 @@ def searching(request):
     return render(request,'search_temp.html', context)
 
 def about_us(request):
-    cus = request.user
-    c = user_cart(cus)
-    context['cart'] = c
+    current_user = request.user
+    current_cart = user_cart(current_user)
+    context['cart'] = current_cart
     return render(request, 'about_us.html', context)
 
 def contacts(request):
-    cus = request.user
-    c = user_cart(cus)
-    context['cart'] = c
+    current_user = request.user
+    current_cart = user_cart(current_user)
+    context['cart'] = current_cart
     return render(request, 'contact.html', context)
 
 def checkout(request):
-    cus = request.user
-    c = user_cart(cus)
-    context['cart'] = c
+    current_user = request.user
+    current_cart = user_cart(current_user)
+    context['cart'] = current_cart
     context['baby_size'] = Baby.objects.all()
     context['boy_size'] = Boy.objects.all()
     context['girl_size'] = Girl.objects.all()
@@ -211,9 +208,9 @@ class Make_order(View):
     
     def post(self,request, *args, **kwargs):
         form = OrderForm(request.POST or None)
-        cus = request.user
-        c = user_cart(cus)
-        cart = c
+        current_user = request.user
+        current_cart = user_cart(current_user)
+        cart = current_cart
         if form.is_valid():
             new_order = form.save(commit=False)
             new_order.customer = cart.customer
